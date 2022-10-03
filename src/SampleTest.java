e3926553275b4c589657d6e862f6087a92140e18
kumaresh <kumareshkumareee75@gmail.com>
2022-10-04 00:36:43 +0530

    DFG

--- src/SampleTest.java
+++ src/SampleTest.java
@@ -1,12 +1,13 @@
 
 public class SampleTest {
 	private void kumaresh() {
-		System.out.println("12345678");
+		System.out.println("12");
 		
 
 	}
 	private void kumaresh1() {
 		System.out.println("hvjfbklfnl");
+		System.out.println("ghjkl");
 	}
 
 }
